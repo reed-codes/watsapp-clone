b2fb45ef9063fe3de4e7ef5e368b6e9a096d1637
reed <helloreedemer@gmail.com>
2021-12-24 10:14:46 +0200

    fixed ui bug

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -9,28 +9,33 @@ const SignIn = () => {
         <Box
             className="fixed bottom-0 flex justify-center items-center flex-col w-full h-full overflow-hidden bg-[#001b3c]"
         >
-            <img src="/login-artwork.png" 
-                 className="w-[300px] h-[300px] object-cover mb-10 rounded-full pointer-events-none"
-                 />
+            <img src="/login-artwork.png"
+                className="w-[300px] h-[300px] object-cover mb-10 rounded-full pointer-events-none"
+            />
 
 
             <Button
                 variant="contained"
                 className="p-0 justify-center items-center overflow-hidden w-[290px] bg-black rounded-lg"
-                sx = {{boxShadow:20}}
+                sx={{
+                    boxShadow: 20,
+                    padding: 0
+                }}
                 onClick={signInWithGoogle}
             >
                 <Box component="span" className="bg-white p-4 h-full min-w-[80px] flex items-center justify-center">
-                <GoogleIcon className="text-[red]" />
+                    <GoogleIcon className="text-[red]" />
                 </Box>
-                <Box component="span" className="font-bold p-4 h-full w-full bg-[#049be4] flex items-center justify-center text-white">
+                <Box component="span"
+                    className="font-bold p-4 h-full w-full bg-[#049be4] flex items-center justify-center text-white"
+                >
                     Sign in with google
                 </Box>
             </Button>
 
-            <img src="/telegram-logo.png" 
-                 className="w-[60px] h-[60px] object-contain mb-4 rounded-lg animate-bounce absolute bottom-5 w-full left-0 right-0 m-auto pointer-events-none"
-                 />
+            <img src="/telegram-logo.png"
+                className="w-[60px] h-[60px] object-contain mb-4 rounded-lg animate-bounce absolute bottom-5 w-full left-0 right-0 m-auto pointer-events-none"
+            />
         </Box>
     )
 }
